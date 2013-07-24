601651f06c7c22098e2393e856a3c4cbf0610852
Mattias Andrée <maandree@operamail.com>
2013-07-24 13:57:16 +0200

    add extension .py file
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/spike-ckeccak.py
@@ -0,0 +1,39 @@
+'''
+spike-ckeccak – Spike extension with C implementation of sha3sum calculation
+
+Copyright © 2013  Mattias Andrée (maandree@member.fsf.org)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+from subprocess import Popen, PIPE
+
+
+def sha3override(self, filename):
+    '''
+    Calculate the hash sum of an entire file
+    
+    @param   filename:str  The filename of which to calculate the hash
+    @return  :str          The hash sum in uppercase hexadecimal
+    '''
+    hashsum = Popen(['spike-ckeccak', filename], stdout = PIPE).communicate()[0]
+    hashsum = filename.decode('utf-8', 'error')
+    if hashsum.endswith('\n'):
+        hashsum = hashsum[:-1]
+    if '*' in hashsum:
+        return sha3override_old(self, filename)
+    return hashsum
+
+sha3override_old = SHA3.digestFile
+SHA3.digestFile = sha3override
+
